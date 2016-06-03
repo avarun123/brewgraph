4fe57e8dd3db637f7ae1af8747690a47b4afe964
Arun Veettil <aveettil@aveettil019859.starbucks.net>
2016-06-03 13:03:23 -0700

    updated code with time decay

--- src/main/scala/com/sbux/loyalty/brewgraph/main/Main.scala
+++ src/main/scala/com/sbux/loyalty/brewgraph/main/Main.scala
@@ -1,111 +1,321 @@
 package com.sbux.loyalty.brewgraph.main
-import org.apache.spark.SparkConf
-import java.io.PrintWriter
-import java.io.FileWriter
-import org.apache.spark.SparkContext
-import scalaj.http.Http
-import scala.collection.mutable.ListBuffer
+
 import java.util.Calendar
+import scala.collection.mutable.ListBuffer
+import org.apache.commons.cli.BasicParser
+import org.apache.commons.cli.HelpFormatter
+import org.apache.commons.cli.Options
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD.rddToPairRDDFunctions
+import java.text.SimpleDateFormat
+import java.util.concurrent.TimeUnit
+import java.time.LocalDate
+import scala.math.pow
+import java.util.Date
+
+/*
+ * computes product to product associations.
+ * command line arguments 
+ * in - input file which contains country,region,timeofday,guid/txid,item,qty
+ * out - output file location in hdfs/s3
+ * c - index of position of country column in the input raw. indexes starts from 0.
+ * r - index of position of region column in the input raw. indexes starts from 0.
+ * t - index of position of timeofday column in the input raw. indexes starts from 0.
+ * i - index of position of item column in the input raw. indexes starts from 0.
+ * u - index of position of user/txid column in the input raw. indexes starts from 0.
+ * 
+ * example command line run in spark
+ * nohup spark-submit --class com.sbux.loyalty.brewgraph.main.Main 
+ * --master yarn-client --deploy-mode client --driver-memory 20g --executor-memory 15g 
+ * --num-executors 6 --executor-cores 4 --conf spark.kryoserializer.buffer.max=512m 
+ * --conf spark.driver.maxResultSize=10g /home/aveettil/brewgraph-1.0.jar 
+ * -in hdfs:///user/aveettil/mop-train-pos-basket -out hdfs:///user/aveettil/brewgraph-out-basket -u 0 -c 1 -r 2 -i 4 -t 6 > out.txt &
+ */
 object Main {
-  val output :  ListBuffer[(String,Double,Double,Double)] = ListBuffer()
-  def main(args : Array[String]) {
-//    var s = Http("http://host.openapi.starbucks.com/product/v1/products/us/11006200").param("locale", "en-us").param("format","json").asString
-//    println(s)
+  var indexOfCountry = 0
+  var indexOfRegion = 1
+  var indexOfTimeOfDay = 5
+  var indexOfItem = 3
+  var indexOfUser = 2
+  var inputFileName = "/projects/mop/mop-train-pos-basket-short.csv"
+  var outFileName = "/projects/sim/output-" + Calendar.getInstance().getTime() + ".txt"
+  var alpha:Double =0.0
+  var indexOfTxDay = 6
+  var indexOfItemCount = 7
+  var dateFormat:SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd")
+  var delimiter = ","
+  var endDay:Date = null
+  val output: ListBuffer[(String, Double, Double, Double)] = ListBuffer()
+  def main(args: Array[String]) {
+    
     val conf = new SparkConf().setMaster("local").setAppName("BrewGraph");
     val sc = new SparkContext(conf);
-    
-//    val mopProducts = sc.textFile("/aveettil/mopcatalog/MOP_products")
-//    val mapSkuToDescrMapping = mopProducts.map{ line => {val split=line.split(","); return (split(1),split(0))}}
-//    val brodcastedMap = sc.broadcast(mapSkuToDescrMapping)
-    
-    
-    var textFile = sc.textFile("/projects/mop/input_midday.csv")
-    var outFile = "/projects/sim/output-"+Calendar.getInstance().getTime()+".txt"
-    if(args.length > 0) {
-      textFile = sc.textFile(args(0))
-      outFile = args(1)
-    }
-    
-   // val textFile = sc.textFile("hdfs://bda01-clu-ns/user/aveettil")
-    
-   // println(y1(0))
-    val counts = textFile.map(line => pairSplit(line)).groupByKey()
-   // textFile.map { line => line.split(",")}.map(word=>
-   // val y = counts.collect()
-    
-    
-    
-    //val y_grouped = y.groupBy({case (key, value) =>key})
-    
-    counts foreach {case (key, value) =>computeJaccard(key,value.toArray)}
-    
-    //output foreach ( pair => println(pair._1+"-->"+pair._2))
-    sc.parallelize(output.toList).map(x=> x._1+","+x._2+","+x._3+","+x._4).saveAsTextFile(outFile)
-    
+
+    // updates the class variables (indexOfCountry,indexOfCountry, etc) if a value is specified as command arguments
+    getOptions(args)
+    var inputFile = sc.textFile(inputFileName);
+
+    val counts = inputFile.map(line => pairSplit(line, indexOfCountry, indexOfRegion, indexOfTimeOfDay)).groupByKey()
+
+   // counts foreach { case (key, value) => computeJaccard(key, value.toArray, indexOfItem, indexOfUser) }
+    counts foreach { case (key, value) => computeJaccardWithTimeDecay (key, value.toArray, indexOfItem, indexOfUser,indexOfTxDay,indexOfItemCount) }
+
+    sc.parallelize(output.toList).map(x => x._1 + "," + x._2 + "," + x._3 + "," + x._4).saveAsTextFile(outFileName)
+
   }
-  def pairSplit(x:String):(String,String) = {
-    val splitLine = x.split(",")
-   
-    return (splitLine(0)+","+splitLine(1)+","+splitLine(5), x)
+  def help(options: Options) = {
+
+    var formater = new HelpFormatter();
+
+    formater.printHelp("Main", options);
+
+    System.exit(0);
+  }
+
+  def pairSplit(x: String, indexOfCountry: Integer, indexofregion: Integer, indexOfTimeOfDay: Integer): (String, String) = {
+    val splitLine = x.split(delimiter)
+    val country = splitLine(indexOfCountry);
+    val region = splitLine(indexofregion);
+    val timeofday = splitLine(indexOfTimeOfDay);
+    return (country + delimiter + region + delimiter + timeofday, x)
     //return ("1","2")
   }
-  def computeJaccard(key:String,values:Array[String])   = {
+  
+  /*
+   * computes Jaccard coefficient between two vectors and also appends it to a list
+   */
+  def computeJaccard(key: String, values: Array[String], indexofItem: Integer, indexOfcolumnvalue: Integer) = {
     println(key)
-    
-  // val itemVectors = values.groupBy ({ x => x.split(",")(1) }).mapValues{value => value.map{x=>x.split(",")(0).concat(",").concat(x.split(",")(2))}}
-     val itemVectors = values.groupBy ({ x => x.split(",")(3) }).mapValues{value => value.map{x=>x.split(",")(2)}}
-  //  val output:ListBuffer[(String,Double)] = ListBuffer()
-   val itemVectors_new = itemVectors.foreach {case (key1,value1)=> {
-      itemVectors.map {case (key2,value2)=> {
-           // 
-        val score:(String,Double,Double,Double) = getOutputTuple(key,key1,key2,value1,value2)
-        output += score
-        //val output1:Map[String,Double] = Map()
-        //output1 + ("2"->2.0)
-        //print (output1.toString())
-      }
-      }
-    
-      
-//      itemVectors_new.foreach {case _:Map[String,Double] => {
-//        println(key)
-//      }
-//      }
-    // println(key .concat("-----").concat(value.mkString(",")))
-     } 
-   }
-   var limit = itemVectors.size
-    
-   // compute pairwise similarity
-//   for (i<- 1 to limit) {
-//     for (j <- i+1 to limit -1) {
-//       
-//     }
-//   }
-   // compute pairwise jaccard
-   //sc.parallelize(output).saveAsTextFile("/projects/sim/output1.txt");
-    
-  }
-  
-  def getOutputTuple (locationkey:String,key1:String,key2:String,value1:Array[String],value2:Array[String]) : (String,Double,Double,Double) = {
-         if (key1 != key2) {
-           val jaccardSimilarity = getJaccard(value1,value2)
-           //return jaccardSimilarity
-           return (locationkey.concat(",").concat(key1).concat(",").concat(key2),jaccardSimilarity._3,jaccardSimilarity._1,jaccardSimilarity._2)
-          // println("%s %s %f %f %f".format(key1,key2,jaccardSimilarity._3,jaccardSimilarity._1,jaccardSimilarity._2))
-        } else {
-          return (locationkey.concat(",").concat(key1).concat(",").concat(key2),1.0,value1.length,value1.length)
+
+    val itemVectors = values.groupBy({ x => x.split(delimiter)(indexofItem) }).mapValues { value => value.map { x => x.split(delimiter)(indexOfcolumnvalue) } }
+
+    val itemVectors_new = itemVectors.foreach {
+      case (key1, value1) => {
+        itemVectors.map {
+          case (key2, value2) => {
+            // 
+            val score: (String, Double, Double, Double) = getOutputTuple(key, key1, key2, value1, value2)
+            output += score
+          }
         }
+
+      }
+    }
+    var limit = itemVectors.size
+
   }
   
-   def getJaccard(row1:Array[String], row2:Array[String]) : (Double,Double,Double) = {
-       var row1_set = row1.toSet
-       var row2_set = row2.toSet
-       var intersection = row1_set.intersect(row2_set).size.toFloat
-       
-       var union = row1_set.union(row2_set).size.toFloat
-       return ( intersection,union,intersection/union)
-    
+  /*
+   * computes Jaccard coefficient between two vectors and also appends it to a list
+   */
+  def computeJaccardWithTimeDecay(key: String, values: Array[String], indexofItem: Integer, indexOfcolumnvalue: Integer,indexTxDate: Integer,indexCount:Integer) = {
+    println(key)
+
+    val itemVectors = values.groupBy({ x => x.split(delimiter)(indexofItem) }).mapValues { value => value.map { x => appendTxDay(x, indexOfcolumnvalue, indexTxDate, indexCount) } }
+
+    val itemVectors_new = itemVectors.foreach {
+      case (key1, value1) => {
+        itemVectors.map {
+          case (key2, value2) => {
+            // 
+            val score: (String, Double, Double, Double) = getOutputTupleWithTimeDecay(key, key1, key2, value1, value2)
+            output += score
+          }
+        }
+
+      }
+    }
+    var limit = itemVectors.size
+
   }
- 
+  
+  def appendTxDay(x:String,indexOfcolumnvalue: Integer,indexTxDate: Integer,indexCount:Integer):(String,Int) =  {
+        var split = x.split(delimiter)
+        var retValue = (split(indexOfcolumnvalue)+";"+split(indexTxDate),split(indexCount).toInt)
+        return retValue;
+  }
+
+  /**
+   * creates pairwise similarity
+   */
+  def getOutputTuple(locationkey: String, key1: String, key2: String, value1: Array[String], value2: Array[String]): (String, Double, Double, Double) = {
+    if (key1 != key2) {
+
+      // you can call other similarity functions instead of jaccard here
+      val jaccardSimilarity = getJaccard(value1, value2)
+      //return jaccardSimilarity - key item1,item2 value jaccard similarity with counts for union and intersection
+      
+      // note - Code can be updated to weed out similarities which does not have minimum threshold for intersection and union
+      return (locationkey.concat(delimiter).concat(key1).concat(delimiter).concat(key2), jaccardSimilarity._3, jaccardSimilarity._1, jaccardSimilarity._2)
+
+    } else {
+          return (locationkey.concat(delimiter).concat(key1).concat(delimiter).concat(key2), 1.0, value1.length, value1.length)
+    }
+  }
+  
+   /**
+   * creates pairwise similarity
+   */
+  def getOutputTupleWithTimeDecay(locationkey: String, key1: String, key2: String, value1: Array[(String,Int)], value2: Array[(String,Int)]): (String, Double, Double, Double) = {
+    if (key1 != key2) {
+
+      // you can call other similarity functions instead of jaccard here
+      val jaccardSimilarity = getJaccardWithTimeDecay(value1, value2,alpha)
+      //return jaccardSimilarity - key item1,item2 value jaccard similarity with counts for union and intersection
+      
+      // note - Code can be updated to weed out similarities which does not have minimum threshold for intersection and union
+      return (locationkey.concat(delimiter).concat(key1).concat(delimiter).concat(key2), jaccardSimilarity._3, jaccardSimilarity._1, jaccardSimilarity._2)
+
+    } else {
+          return (locationkey.concat(delimiter).concat(key1).concat(delimiter).concat(key2), 1.0, value1.length, value1.length)
+    }
+  }
+
+  /**
+   * computes Jaccard coefficient between two vectors
+   * Jaccard's similarity = c/(a+b-c) where c= number of bits common to bothrow1 and row2
+     a = number of 1s in row1, b=number of 1s in 
+   */
+  def getJaccard(row1: Array[String], row2: Array[String]): (Double, Double, Double) = {
+    var row1_set = row1.toSet
+    var row2_set = row2.toSet
+    var intersection = row1_set.intersect(row2_set).size.toFloat
+
+    var union = row1_set.union(row2_set).size.toFloat
+    return (intersection, union, intersection / union)
+
+  }
+  
+  /**
+   * computes Jaccard coefficient between two vectors
+   * Jaccard's similarity = c/(a+b-c) where c= number of bits common to bothrow1 and row2
+     a = number of 1s in row1, b=number of 1s in 
+     to add time decay, instead of adding up 1s, add time decay value for computing a,b,c
+   */
+  def getJaccardWithTimeDecay(row1: Array[(String,Int)], row2: Array[(String,Int)],alpha:Double): (Double, Double, Double) = {
+    
+    var set = collection.mutable.Set[String]()
+    
+    var union = 0.0
+    row1.foreach( { case(key,value) => {
+      set+=key
+      var decay = pow(Math.E,-1 * getTimeDeltaDays(key.split(";")(1)) * alpha) // multiply with time alpha
+      union+= decay
+    }
+    })
+    var intersection = 0.0
+     row2.foreach( { case(key,value) => {
+       var decay = pow(Math.E,-1 * getTimeDeltaDays(key.split(";")(1)) * alpha) // multiply with time alpha
+        union+= decay
+        if (set.contains(key)){
+         
+          intersection += decay
+         
+        }
+       
+    }
+    })
+    union = union - intersection
+    return (intersection, union, intersection / union)
+  }
+    
+    def getTimeDeltaDays(date:String):Long = {
+     
+      var date1 = dateFormat.parse(date).getTime()
+       
+      var diffInMillis:Long = endDay.getTime()-date1
+      var tu:TimeUnit =TimeUnit.DAYS
+      var retValue =  tu.convert(diffInMillis,TimeUnit.MILLISECONDS)
+      return retValue
+      
+    }
+  
+
+
+  def getOptions(args: Array[String]) = {
+
+    val options = new Options();
+
+    
+    options.addOption("in", true, "input file");
+
+    options.addOption("out", true, "output file");
+
+    options.addOption("c", true, "index of country");
+
+    options.addOption("r", true, "index of region");
+
+    options.addOption("t", true, "index of time of day");
+
+    options.addOption("i", true, "index of item");
+
+    options.addOption("u", true, "index of user");
+    
+    options.addOption("alpha", true, "time decay parameter");
+    
+    options.addOption("dt", true, "index of date of transaction");
+    
+    options.addOption("n", true, "index of item count");
+    
+    options.addOption("del", true, "delimiter");
+    options.addOption("end", true, "delimiter");
+
+    val parser = new BasicParser();
+
+    val cmd = parser.parse(options, args);
+
+    if (cmd.hasOption("h")) {
+      help(options)
+    }
+
+    if (cmd.hasOption("in")) {
+      inputFileName = cmd.getOptionValue("in")
+    }
+
+    val allOptions = cmd.getOptions()
+
+    if (cmd.hasOption("out")) {
+
+      outFileName = cmd.getOptionValue("out")
+    }
+
+    if (cmd.hasOption("c")) {
+
+      indexOfCountry = cmd.getOptionValue("c").toInt
+    }
+    if (cmd.hasOption("r")) {
+      indexOfRegion = cmd.getOptionValue("r").toInt
+    }
+    if (cmd.hasOption("t")) {
+      indexOfTimeOfDay = cmd.getOptionValue("t").toInt
+    }
+    if (cmd.hasOption("i")) {
+      indexOfItem = cmd.getOptionValue("i").toInt
+    }
+    if (cmd.hasOption("u")) {
+      indexOfUser = cmd.getOptionValue("u").toInt
+    }
+    
+    if (cmd.hasOption("alpha")) {
+      alpha = cmd.getOptionValue("alpha").toDouble
+    }
+    
+    if (cmd.hasOption("dt")) {
+      indexOfTxDay = cmd.getOptionValue("dt").toInt
+    }
+     if (cmd.hasOption("n")) {
+      indexOfItemCount = cmd.getOptionValue("n").toInt
+    }
+    if (cmd.hasOption("del")) {
+      delimiter = cmd.getOptionValue("del")
+    }
+    if (cmd.hasOption("end")) {
+      var sEndDay = cmd.getOptionValue("end")
+      endDay = dateFormat.parse(sEndDay)
+    } else {
+      endDay = new Date();
+    }
+  }
+
 }
